21b6ca1c91f97b08057ac4349243f24b3c145eaf
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 21:28:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-    new_array = [ "a", "b", "c", "d"]
-    new_array.unshift("wow")
+    new_array = [ "a", "b", "c", "d", "arrays!"]
+    new_array.pop("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
